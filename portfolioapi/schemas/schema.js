dd816fb9be1a520bc949ed399ce5ed1f182d575a
Luís Leandro <luisleandro.wd@gmail.com>
2022-12-07 00:25:12 +0000

    preparing for vercel deployment. remove portfolioapi

--- portfolioapi/schemas/schema.js
+++
@@ -1,9 +0,0 @@
-import createSchema from 'part:@sanity/base/schema-creator';
-
-import schemaTypes from 'all:part:@sanity/base/schema-type';
-import { linkModel, projectModel } from '../models/work';
-
-export default createSchema({
-  name: 'default',
-  types: schemaTypes.concat([projectModel, linkModel]),
-});
